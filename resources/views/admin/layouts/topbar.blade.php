bcb2ac9012118aceb61e260d66394b17c5a22625
davidluengos <davidluengosanchez@hotmail.com>
2024-10-07 19:56:02 +0200

    añadir bloque de mensajes de sesión debajo del topbar

--- resources/views/admin/layouts/topbar.blade.php
+++ resources/views/admin/layouts/topbar.blade.php
@@ -194,3 +194,15 @@
 
 </nav>
 <!-- End of Topbar -->
+
+<!-- Añadimos aquí los posibles mensajes del sistema, añadiendo un botón para quitarlo manualmente -->
+@if (session('success'))
+    <div class="container">
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    </div>
+@endif
